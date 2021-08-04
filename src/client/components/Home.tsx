4177c435b4da5251821cf0028abb65d4b84bb03d
Biprajit Majumder <biprajit163@gmail.com>
2021-08-04 16:51:55 -0400

    github link and linkedIn link added

--- src/client/components/Home.tsx
+++ src/client/components/Home.tsx
@@ -1,13 +1,56 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { AiFillLinkedin, AiOutlineGithub } from 'react-icons/ai';
 
 export const Home: FC = () => {
     return (
         <div>
-            <h1>Welcome to the home page</h1>
+            <div className="title-container">
+                <h1>Poker Roulette</h1>
+                <p>Get the most points in a minute</p>
+            </div>
             <Link to="/game">
                 <button className="btn btn-primary">Play Game</button>
             </Link>
+
+            <div className="rules-section">
+                This is a single player game with no AI, you can sign up two players
+                but you can control each players actions.
+            </div>
+
+            <div className="profile-info">
+                <a 
+                    href="https://www.linkedin.com/in/biprajit-majumder/"
+                    style={{
+                        textDecoration: 'none',
+                        color: 'inherit'
+                    }}
+                    target="_blank"
+                >
+                    <AiFillLinkedin 
+                        style={{
+                            height: '3em',
+                            width: '3em',
+                        }}
+                    />
+                </a>
+                
+                <a 
+                    href="https://github.com/biprajit163"
+                    style={{
+                        textDecoration: 'none',
+                        color: 'inherit'
+                    }}
+                    target="_blank"
+                >
+                    <AiOutlineGithub 
+                        style={{
+                            height: '3em',
+                            width: '3em',
+                        }}
+                    />
+                </a>
+            </div>
         </div>
     );
 }
