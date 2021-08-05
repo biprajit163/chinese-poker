cfd659ea44f5fa8ad212bceba9eb4f3a8142ba4a
Biprajit Majumder <biprajit163@gmail.com>
2021-08-04 22:42:50 -0400

    take out comment secrion from server/server.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,6 @@
 "use strict";
 
 
-/* --------------------------------------------------------------------------------------- 
-- use express and socket.io
-- setup http server and socket.io connection
-- setup socket.io-client so server and client can communicate
-- userroom concept on socket.io docs on user.js
-    - create remote rooms from users on client side
-    - send a "room code" from client side and use it to create a room on the server side 
-    - use random query generator and query params for this.
-
-** Important Links:
-- Getting started with TS and Socket.io
-    - https://tutorialedge.net/typescript/typescript-socket-io-tutorial/ 
-- Socket.io Client API
-    - https://socket.io/docs/v3/client-api/index.html#Socket 
-- Socket.io Server API
-    - https://socket.io/docs/v3/server-api/index.html 
-
----------------------------------------------------------------------------------------- */
-
 const { 
     addPlayer, 
     getPlayer, 
